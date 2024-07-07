f4e42d37c5135ce6e43607df88de7fab27e4a262
userman89 <graph-design89@mail.ru>
2024-07-07 08:32:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,69 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const WebSocket = require('ws');
-const path = require('path');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const nunjucks = require("nunjucks");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const WebSocket = require("ws");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const cookieParser = require("cookie-parser");
 
-const User = require('./models/User');
-const Timer = require('./models/Timer');
+const User = require("./models/User");
+const Timer = require("./models/Timer");
+const http = require('http');
 
-require('dotenv').config();
+require("dotenv").config();
+
+// Получение и логирование внешнего IP-адреса
+http.get('http://ifconfig.me', (res) => {
+  res.on('data', (chunk) => {
+    console.log(`External IP Address: ${chunk}`);
+  });
+});
 
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app); // HTTP server for WebSocket
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 45000,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    if (err.reason) {
-      console.error('Reason:', err.reason);
-    }
-  });
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Таймаут выбора сервера
+    socketTimeoutMS: 45000, // Таймаут сокета
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 // Configure Nunjucks
-nunjucks.configure('views', {
+nunjucks.configure("views", {
   autoescape: true,
   express: app,
   tags: {
-    blockStart: '[%',
-    blockEnd: '%]',
-    variableStart: '[[',
-    variableEnd: ']]',
-    commentStart: '[#',
-    commentEnd: '#]',
+    blockStart: "[%",
+    blockEnd: "%]",
+    variableStart: "[[",
+    variableEnd: "]]",
+    commentStart: "[#",
+    commentEnd: "#]",
   },
 });
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*", // Разрешить доступ с любого домена
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Разрешить передачу куки
+  }),
+);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.set('view engine', 'njk');
-app.set('views', path.join(__dirname, 'views'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 // Session configuration
 const sessionMiddleware = session({
@@ -67,37 +72,51 @@ const sessionMiddleware = session({
   saveUninitialized: false,
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI,
-    collectionName: 'sessions',
+    collectionName: "sessions",
   }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
     httpOnly: true,
   },
 });
 
 app.use(sessionMiddleware);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "njk");
+app.set("views", path.join(__dirname, "views"));
+
 // JWT authentication middleware
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  console.log("Authorization Header:", authHeader);
+
   if (authHeader) {
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(" ")[1];
+    console.log("Token:", token);
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
+        console.error("JWT verification error:", err);
         return res.sendStatus(403); // Forbidden
       }
+      console.log("JWT verification success:", user);
       req.user = user;
       next();
     });
   } else {
+    console.error("Authorization header missing");
     res.sendStatus(401); // Unauthorized
   }
 };
 
 // Registration endpoint
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
+
   try {
     const existingUser = await User.findOne({ username });
     if (existingUser) {
@@ -108,9 +127,12 @@ app.post('/signup', async (req, res) => {
     const user = new User({ username, password: hashedPassword });
     await user.save();
 
-    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+    const token = jwt.sign(
+      { _id: user._id, username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+    );
     req.session.user = user;
-    res.render('index', { user, userToken: token });
+    res.render("index", { user, userToken: token });
   } catch (error) {
     console.error("Error during registration:", error);
     res.status(500).send("An error occurred during user registration");
@@ -118,35 +140,46 @@ app.post('/signup', async (req, res) => {
 });
 
 // Login endpoint
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
 
-  if (user && await user.comparePassword(password)) {
-    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+  if (user && (await user.comparePassword(password))) {
+    const token = jwt.sign(
+      { _id: user._id, username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+    );
     req.session.user = user;
 
-    res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-    res.render('index', { user: user, userToken: token });
+    // Отправка токена клиенту в заголовке Set-Cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.render("index", { user: user, userToken: token });
   } else {
     res.redirect("/");
   }
 });
 
 // Logout endpoint
-app.get('/logout', (req, res) => {
-  req.session.destroy(err => {
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
     if (err) {
-      console.error('Error destroying session:', err);
+      console.error("Error destroying session:", err);
       res.status(500).send("An error occurred during logout");
     } else {
-      res.render('index', { user: null, userToken: null });
+      res.render("index", { user: null, userToken: null });
     }
   });
 });
 
+app.get("/", (req, res) => {
+  res.render("index"); // Предполагается, что у вас есть файл 'index.njk' в папке 'views'
+});
+
 // Create new timer
-app.post('/timer', authenticateJWT, async (req, res) => {
+app.post("/timer", authenticateJWT, async (req, res) => {
   const { description } = req.body;
 
   try {
@@ -164,31 +197,31 @@ app.post('/timer', authenticateJWT, async (req, res) => {
       timer: newTimer,
     });
   } catch (error) {
-    console.error('Error creating timer:', error);
-    res.status(500).json({ error: 'An error occurred while creating timer' });
+    console.error("Error creating timer:", error);
+    res.status(500).json({ error: "An error occurred while creating timer" });
   }
 });
 
 // Handle stopping timer
-app.post('/timer/stop/:id', authenticateJWT, async (req, res) => {
+app.post("/timer/stop/:id", authenticateJWT, async (req, res) => {
   const timerId = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(timerId)) {
-    return res.status(400).json({ error: 'Invalid timer ID' });
+    return res.status(400).json({ error: "Invalid timer ID" });
   }
 
   try {
     const timer = await Timer.findById(timerId);
     if (!timer) {
-      return res.status(404).json({ error: 'Timer not found' });
+      return res.status(404).json({ error: "Timer not found" });
     }
 
     if (timer.userId.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ error: 'Access to this timer is denied' });
+      return res.status(403).json({ error: "Access to this timer is denied" });
     }
 
     if (!timer.isActive) {
-      return res.status(400).json({ error: 'Timer is already stopped' });
+      return res.status(400).json({ error: "Timer is already stopped" });
     }
 
     timer.isActive = false;
@@ -196,77 +229,80 @@ app.post('/timer/stop/:id', authenticateJWT, async (req, res) => {
     timer.durationInSeconds = Math.floor((timer.end - timer.start) / 1000);
     await timer.save();
 
-    res.json({ message: 'Timer stopped successfully', timer });
+    res.json({ message: "Timer stopped successfully", timer });
   } catch (error) {
-    console.error('Error stopping timer:', error);
-    res.status(500).json({ error: 'An error occurred while stopping timer' });
+    console.error("Error stopping timer:", error);
+    res.status(500).json({ error: "An error occurred while stopping timer" });
   }
 });
 
 // Endpoint to update timers
-app.get('/timer/update', authenticateJWT, async (req, res) => {
+app.get("/timer/update", authenticateJWT, async (req, res) => {
   const userId = req.user._id;
   try {
-    const timers = await Timer.find({ userId }).lean();
-    timers.forEach(timer => {
+    const timers = await Timer.find({ userId }).lean(); // Использование lean для уменьшения накладных расходов
+    timers.forEach((timer) => {
       if (timer.isActive) {
         timer.durationInSeconds = Math.floor((new Date() - timer.start) / 1000);
       }
     });
     res.json({ timers });
   } catch (error) {
-    console.error('Error fetching timers:', error);
-    res.status(500).json({ error: 'An error occurred while fetching timers' });
+    console.error("Error fetching timers:", error);
+    res.status(500).json({ error: "An error occurred while fetching timers" });
   }
 });
 
 // WebSocket server
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws, req) => {
+wss.on("connection", (ws, req) => {
   sessionMiddleware(req, {}, () => {
     if (req.session.user) {
-      console.log('WebSocket connected with user:', req.session.user.username);
+      console.log("WebSocket connected with user:", req.session.user.username);
 
-      ws.on('message', (message) => {
+      ws.on("message", (message) => {
         console.log(`Received message => ${message}`);
       });
 
-      ws.on('close', () => {
-        console.log('WebSocket disconnected');
+      ws.on("close", () => {
+        console.log("WebSocket disconnected");
       });
     } else {
-      console.log('Unauthorized WebSocket connection');
-      ws.close(1000, 'Unauthorized');
+      console.log("Unauthorized WebSocket connection");
+      ws.close(1000, "Unauthorized");
     }
   });
 });
 
-// Function to send timer updates
+// Функция для отправки обновлений таймеров
 const sendTimersUpdate = async () => {
   try {
-    const timers = await Timer.find({}).lean();
+    const timers = await Timer.find({});
 
-    timers.forEach(timer => {
+    timers.forEach((timer) => {
       if (timer.isActive) {
         timer.durationInSeconds = Math.floor((new Date() - timer.start) / 1000);
       }
     });
 
-    wss.clients.forEach(client => {
+    const timersData = JSON.stringify(timers);
+
+    wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ timers }));
+        client.send(timersData);
       }
     });
   } catch (error) {
-    console.error('Error sending timer updates:', error);
+    console.error("Error fetching timers:", error);
   }
 };
 
-// Start sending timer updates every second
+// Запускаем интервал для отправки обновлений каждые 1 секунду
 setInterval(sendTimersUpdate, 1000);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
